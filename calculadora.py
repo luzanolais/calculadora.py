8330ca424c17426960d44ca27d56716edeeaa705
luzanolais <luzanolais@gmail.com>
2026-07-07 09:44:07 -0300

    Criar calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+git init
+git config --global user.name "Seu Nome"
+git config --global user.email "seu@email.com"
+
+#mini calculadora 
+#aluno: Lais Luzano
+
+a= 10
+b= 5
+
+soma= a+b
+subtracao= a-b
+multiplicacao= a*b
+divisao= a/b
+
+print(f"{a} + {b} = {soma}")
+print(f"{a} - {b} = {subtracao}")
+print(f"{a} * {b} = {multiplicacao}")
+print(f"{a} / {b} = {divisao}")
